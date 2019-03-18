74461815b13b5936c89ca53877d6f3b3e1aa4d6e
marekt <marek.teuchner@moderntv.eu>
2019-03-18 17:47:38 +0100

    #

---
+++ src/main/java/cz.sledovanitv.java.course.beginners/Main.java
@@ -0,0 +1,16 @@
+package cz.sledovanitv.java.course.beginners;
+
+/**
+ * Main class of the application
+ */
+public class Main {
+
+    /**
+     * Main method - This is the starting point of the application
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
