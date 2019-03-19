94727b15dd125f7d76853aae90a6ebe2c3033bb4
marekt <marek.teuchner@moderntv.eu>
2019-03-19 11:43:02 +0100

    Lesson 1

--- src/main/java/cz.sledovanitv.java.course.beginners/Main.java
+++ src/main/java/cz.sledovanitv.java.course.beginners/Main.java
@@ -5,12 +5,20 @@ package cz.sledovanitv.java.course.beginners;
  */
 public class Main {
 
+    private static void printHelloWorld() {
+        System.out.println("Hello world!");
+    }
+
     /**
      * Main method - This is the starting point of the application
+     *
      * @param args Command line arguments.
      */
     public static void main(String[] args) {
-        
+        // Write "Hello world"
+        printHelloWorld();
+        // Write "Hello world" again!
+        printHelloWorld();
     }
 
 }
